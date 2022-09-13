3a0d3113d3a1155cb9d17f1abd12584bf56519e4
Marcus-Cox <marcus.cox92@gmail.com>
2022-09-12 19:59:48 -0500

    restarted project code

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { Fragment } from "react";
+
+import AllGuidesPage from "./pages/AllGuides";
+import FavoriteGuidesPage from "./pages/FavoriteGuides";
+import NewGuidePage from "./pages/NewGuide";
+
+function App() {
+  //localhost:3000/pathname / is default
+  return (
+    <div>
+      
+        <Fragment>
+          <Routes>
+            
+            <Route exact path="/" element={<AllGuidesPage />}>
+            </Route>
+
+            <Route exact path="/new-guide" element={<NewGuidePage />}>
+              
+            </Route>
+
+            <Route exact path="favorite-guides" element={<FavoriteGuidesPage />}>
+              
+            </Route>
+
+          </Routes>
+        </Fragment>
+      
+    </div>
+  );
+}
+export default App;
