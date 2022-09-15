02c3e2945ee85a5b8c2e1309af3c9f7047f95b41
Marcus-Cox <marcus.cox92@gmail.com>
2022-09-15 17:35:19 -0500

    added localt host to new guide page

--- src/pages/NewReptileGuidePage.js
+++ src/pages/NewReptileGuidePage.js
@@ -1,8 +1,14 @@
 import NewReptileGuideForm from "../components/guides/NewReptileGuideForm";
 
+
+//This Page Handles getting data to make a new post request, i think
+
+// const URL = "http://localhost:8000"
+
 function NewReptileGuidePage() {
   function addReptileGuideHandler(reptileGuideData) {
-    
+    fetch();
+
   }
 
   return (
