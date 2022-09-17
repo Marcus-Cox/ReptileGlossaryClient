f7e539f5ba66a8dbfc276b897fad9815cd832776
Marcus-Cox <marcus.cox92@gmail.com>
2022-09-17 17:10:19 -0500

    publishing date removed from post request. CRUD Viable

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,8 +1,9 @@
-import React from "react"
-import { Route } from "react-router-dom"
+import React from "react";
+import { Route } from "react-router-dom";
 import { ReptileGuideList } from "./reptileguide/ReptileGuideList";
 import { NewReptileGuideForm } from "./reptileguide/ReptileGuideForm";
 import { UpdateGameForm } from "./reptileguide/UpdateReptileGuideForm";
+// import { ReptileGuideDetails } from "/home/marcus/workspace/reptile-glossary/reptile-glossary-client/reptile-glossary/src/components/reptileguide/ReptileGuideDetails.js";
 
 export const ApplicationViews = () => {
   return (
@@ -14,14 +15,18 @@ export const ApplicationViews = () => {
         }}
       >
         <Route exact path={["/", "/reptileguides"]}>
-                <ReptileGuideList />
-            </Route>
-            <Route exact path="/reptileguides/new">
-                <NewReptileGuideForm />
-            </Route>
-            <Route exact path="/reptileguides/:reptileguideId/update">
-                <UpdateGameForm/>
-            </Route>
+          <ReptileGuideList />
+        </Route>
+        <Route exact path="/reptileguides/new">
+          <NewReptileGuideForm />
+        </Route>
+        <Route exact path="/reptileguides/:reptileguideId/update">
+          <UpdateGameForm />
+        </Route>
+
+        {/* <Route exact path="/reptileguides/:reptileguidesId">
+          <ReptileGuideDetails />
+        </Route> */}
       </main>
     </>
   );
